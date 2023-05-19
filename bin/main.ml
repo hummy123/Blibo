2efd369b9e29d64c036da2553ef48f9b1a136003
humza <humza@pop-os.localdomain>
2023-05-19 11:13:20 +0100

    add fold for benchmaring

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,16 @@
-module MapInt = Map.Make (String)
+module MapString = Map.Make (String)
 
 let build_map () =
   let rec loop num map =
     if num = 10_000 then map
     else
       let key = string_of_int num in
-      let map = MapInt.add key num map in
+      let map = MapString.add key num map in
       loop (num + 1) map
   in
-  loop 0 MapInt.empty
+  loop 0 MapString.empty
+
+let map_fold map = MapString.fold (fun _ _ _ -> ()) map ()
 
 let build_btree () =
   let rec loop num tree =
@@ -20,17 +22,33 @@ let build_btree () =
   in
   loop 0 Leaf
 
-let () =
-  Printf.printf "B-Tree:";
-  let start_time = Sys.time () in
-  let _ = build_btree () in
-  let end_time = Sys.time () in
-  let difference = end_time -. start_time in
-  Printf.printf "\n%f\n\n" difference;
+let btree_fold tree = Blibo.fold (fun _ _ _ -> ()) () tree
 
-  Printf.printf "Map:";
+let () =
+  Printf.printf "\nB-Tree:\n";
   let start_time = Sys.time () in
-  let _ = build_map () in
+  let btree = build_btree () in
   let end_time = Sys.time () in
   let difference = end_time -. start_time in
-  Printf.printf "\n%f\n" difference
+  Printf.printf "%f\n" difference;
+
+  Printf.printf "Map:\n";
+  let start_time = Sys.time () in
+  let map = build_map () in
+  let end_time = Sys.time () in
+  let difference = end_time -. start_time in
+  Printf.printf "%f\n" difference;
+
+  Printf.printf "B-Tree:\n";
+  let start_time = Sys.time () in
+  let _ = btree_fold btree in
+  let end_time = Sys.time () in
+  let difference = end_time -. start_time in
+  Printf.printf "%f\n" difference;
+
+  Printf.printf "Map:\n";
+  let start_time = Sys.time () in
+  let _ = map_fold map in
+  let end_time = Sys.time () in
+  let difference = end_time -. start_time in
+  Printf.printf "%f\n" difference
